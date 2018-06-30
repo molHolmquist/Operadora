9048d18843a9c92225a7e55d73852ec106806f3c
Davi Rolim <davi.delima@hotmail.com>
2018-06-29 23:22:27 -0300

    classe cliente

--- src/operadora/Cliente.java
+++ src/operadora/Cliente.java
@@ -1,7 +1,32 @@
 package operadora;
 
 public class Cliente {
-	private String nome;
+	private String nomeString;
+	private String cpfOuCnpj;
+	private String endereco;
+	//private ArrrayList<Celular> listaCel;
+	
+	
+	public String getNomeString() {
+		return nomeString;
+	}
+	public void setNomeString(String nomeString) {
+		this.nomeString = nomeString;
+	}
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
